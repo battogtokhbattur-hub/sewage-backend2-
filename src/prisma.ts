05be37d08a92897c7a6405c34ed5d612ecf33034
battogtokhbattbattogtokhbattur-hub <Battogtokhbattur@gmail.com>
2026-05-15 16:28:10 +0800

    fix: add prisma accelerate extension

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ReturnType<typeof createClient> | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function createClient() {
+  return new PrismaClient({
     log: ["error", "warn"],
-  });
+  }).$extends(withAccelerate());
+}
+
+export const prisma = globalForPrisma.prisma ?? createClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
